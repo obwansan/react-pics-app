575f39acc936c7d7cd07d8fb22414a1a570b0456
Kevin O'Brien <kob123@hotmail.co.uk>
2019-01-19 12:52:08 +0000

    using promise->then to process http reponse

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
+// The program flow exits the axios.get method before a response is received from the api.
+// So we need a .then function that we pass a callback...that processes the data when it's
+// returned.
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    // http get request
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },  
+      headers: {
+        Authorization: 'Client-ID 746af259ec449cd27e683513f8d561f25869fb0123d756e56f28ca81f31c32d1' 
+      }
+    }).then(response => {
+      console.log(response.data.results);
+    })
   }
 
   render() {
