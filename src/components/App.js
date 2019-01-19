cc849064cadfc4e61f7821886de867629177278b
Kevin O'Brien <kob123@hotmail.co.uk>
2019-01-19 14:24:22 +0000

    added notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// Can use any name for what you import from unsplash.js, just has to match
+// it's use later in the code (unsplash.get). This effectively assigns an 
+// instance of the axios client to the 'variable' named 'unsplash'.
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
